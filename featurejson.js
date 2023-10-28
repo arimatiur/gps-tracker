de1bc5138587745e365d985ffeefdb858c480d1a
Ari Matiur <arimatiur0@gmail.com>
2023-10-28 21:18:24 +0700

    fcgh

---
+++ featurejson.js
@@ -0,0 +1,69 @@
+var features = [
+  {
+    attributes: {
+      OBJECTID: "2",
+      PCODE: "Ari-002",
+      Accuracy: 1595.22766557888,
+      Latitude: -6.2446807,
+      Longitude: 106.924067,
+      Altitude: null,
+      Heading: null,
+      Speed: null,
+      Counter: 0,
+    },
+    geometry: { x: 106.924067, y: -6.2446807 },
+  },
+];
+
+features = [
+  {
+    attributes: {
+      OBJECTID: 2,
+      PCODE: "Ari-002",
+      Accuracy: 1595.22766557888,
+      Latitude: -6.2446807,
+      Longitude: 106.924067,
+      Altitude: 0,
+      Heading: 0,
+      Speed: 0,
+      Counter: 0,
+    },
+    geometry: {
+      x: 106.924067,
+      y: -6.2446807,
+    },
+  },
+];
+
+features = 
+{
+    "geometry":{
+        "x":106.924067,"y":-6.2446807
+    },
+    "attributes":{
+        "OBJECTID":2,
+        "PCODE":"Ari-002",
+        "Accuracy":1595.22766557888,
+        "Latitude":-6.2446807,
+        "Longitude":106.924067,
+        "Altitude":0,
+        "Heading":0,
+        "Speed":0,
+        "Counter":0
+    }
+};
+
+features = {
+  geometry: { x: 106.924067, y: -6.2446807 },
+  attributes: {
+    OBJECTID: 2,
+    PCODE: "Ari-002",
+    Accuracy: 1595.22766557888,
+    Latitude: -6.2446807,
+    Longitude: 106.924067,
+    Altitude: 0,
+    Heading: 0,
+    Speed: 0,
+    Counter: 0,
+  },
+};
